fc823e0fbb3c5e7f4fc122fa3c757f547f13beff
kethan2206 <76050476+kethan2206@users.noreply.github.com>
2022-06-15 17:18:41 +0530

    UI yenveez

---
+++ app/src/main/java/com/example/ui_yenveez/Intro3.java
@@ -0,0 +1,14 @@
+package com.example.ui_yenveez;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Intro3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro3);
+    }
+}
